e59b4d92cf9b186b7bcc052f1f24d3abcea2abd7
studane <zhidaneprabowoguzlend@gmail.com>
2026-08-04 10:12:20 +0700

    membuat prosedur displayAll dari siswa, setnama dari orang, setId dari siswa

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -23,3 +23,15 @@ public:
     void displayAll(orang &a);
 };
 
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
