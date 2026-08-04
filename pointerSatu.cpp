ac0e06e44971bfa649feb2016cdbd0d2ad734d22
studane <zhidaneprabowoguzlend@gmail.com>
2026-08-04 10:36:03 +0700

    membuat program utama yang berisi cara membuat objek

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -8,3 +8,16 @@ class mahasiswa{
     }
 };
 
+int main (){
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); //Member Access Operator
+
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //pointer Dereference pMhs
+    pMhs->nim = 3; //Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
